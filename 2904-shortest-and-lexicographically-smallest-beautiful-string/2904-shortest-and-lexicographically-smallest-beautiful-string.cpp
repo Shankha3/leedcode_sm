8fd18676c4160a7552a49a202896a451d0cdd8c2
Shankhadip Mondal <shankhadipmondal8@gmail.com>
2026-08-26 07:41:42 +0530

    Time: 4 ms (19.14%), Space: 9.3 MB (30.48%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -1,25 +1,36 @@
 class Solution {
 public:
     string shortestBeautifulSubstring(string s, int k) {
-        if (ranges::count(s, '1') < k) {
-            return "";
-        }
-        string ans = s;
-        int cnt = 0;
-        for (int left = 0, right = 0; right < s.length(); right++) {
-            cnt += s[right] - '0';
-            while (cnt > k || s[left] == '0') {
-                cnt -= s[left] - '0';
-                left++;
-            }
-            if (cnt == k) {
-                string t = s.substr(left, right - left + 1);
-                if (t.length() < ans.length() ||
-                    t.length() == ans.length() && t < ans) {
-                    ans = move(t);
+        string ans = "";
+        int n = s.size();
+
+        for (int i = 0; i < n; i++) {
+
+            int oneCnt = 0;
+            string cur = "";
+
+            for (int j = i; j < n; j++) {
+
+                cur += s[j];
+
+                if (s[j] == '1')
+                    oneCnt++;
+
+                if (oneCnt > k)
+                    break;
+
+                if (oneCnt == k) {
+
+                    if (ans == "" ||
+                        cur.size() < ans.size() ||
+                        (cur.size() == ans.size() && cur < ans)) {
+
+                        ans = cur;
+                    }
                 }
             }
         }
+
         return ans;
     }
 };
